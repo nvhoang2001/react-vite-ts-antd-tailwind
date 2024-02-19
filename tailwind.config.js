e9693fbcaabcd53b87d17e4a1544081fb5601ba6
Sotatek-HoangNgo <hoang.ngo@sotatek.com>
2024-02-19 10:52:00 +0700

    Init project

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+import plugin from 'tailwindcss/plugin'
+import aspectRatio from '@tailwindcss/aspect-ratio'
+import formPlugin from '@tailwindcss/forms'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  corePlugins: {
+    preflight: false
+  },
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    extend: {
+      screens: {},
+      animation: {},
+      keyframes: {},
+      colors: {},
+      fontFamily: {},
+      fontSize: {},
+      spacing: {}
+    }
+  },
+  plugins: [
+    aspectRatio,
+    formPlugin,
+    plugin(({ addUtilities }) => {
+      addUtilities({})
+    })
+  ]
+}
